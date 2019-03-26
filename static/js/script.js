51414b8e19132b4fff1a4604867aed62c4308a73
hanneoa <hanneoa@gmail.com>
2019-03-26 20:29:49 +0000

    progress

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,5 @@
 
 
 // This is the slider JavaScript //
-  $("#customRange1").change(function(){
-    var newValue = $('#customRange1').val();
-    console.log(newValue);
-    $("#sliderValue").text(newValue);
-  });
+
 // This is the end of slider JavaScript //
