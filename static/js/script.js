ae299a9dc36f804ea27f950e7293e6c60cc46e90
hanneoa <hanneoa@gmail.com>
2019-03-26 19:55:12 +0000

    Slider... maybe

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+
+
+// This is the slider JavaScript //
+// With JQuery
+$("#ex8").slider({
+	tooltip: 'always'
+});
+
+// Without JQuery
+var slider = new Slider("#ex8", {
+	tooltip: 'always'
+});
+
+// This is the end of slider JavaScript //
