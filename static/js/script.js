0e024b6cc644e3d24c8b4be8828e430b3443bcac
hanneoa <hanneoa@gmail.com>
2019-03-26 20:43:24 +0000

    resolve conflict

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,26 @@
 // This is the slider JavaScript //
 
 // This is the end of slider JavaScript //
+
+// this is the calendar jscode (uses jquery)
+
+$(function() {
+  $( ".calendar" ).datepicker({
+		dateFormat: 'dd/mm/yy',
+		firstDay: 1
+	});
+
+	$(document).on('click', '.date-picker .input', function(e){
+		var $me = $(this),
+				$parent = $me.parents('.date-picker');
+		$parent.toggleClass('open');
+	});
+
+
+	$(".calendar").on("change",function(){
+		var $me = $(this),
+				$selected = $me.val(),
+				$parent = $me.parents('.date-picker');
+		$parent.find('.result').children('span').html($selected);
+	});
+});
